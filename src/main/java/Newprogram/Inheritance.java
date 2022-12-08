03e6db068fa28879e90766383b1f686373b372d8
Madhav Sahu <92218111+MADHAV4045@users.noreply.github.com>
2022-12-08 18:45:33 +0530

    Delete src/main/java/Newprogram directory

--- src/main/java/Newprogram/Inheritance.java
+++
@@ -1,9 +0,0 @@
-package Newprogram;
-
-public class Inheritance extends A{
-    public static void main(String[] args) {
-        Inheritance b=new Inheritance();
-        b.run();
-        b.car();
-    }
-}
